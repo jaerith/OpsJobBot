7952c5acad5f828dc622902f8897640f52aedb6e
jaerith <Aaron.Kendall@gmail.com>
2017-01-27 12:50:31 -0500

    Created a repository layer for the jobs to be monitored and scheduled.

---
+++ OpsJobRepository/OpsJob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsJobRepository
+{
+    public class OpsJob
+    {
+        public OpsJob()
+        { }
+
+        public string JobName { get; set; }
+
+        public string Description { get; set; }
+
+        public string ActiveCd { get; set; }
+
+        public string LastReturnCd { get; set; }
+    }
+}
