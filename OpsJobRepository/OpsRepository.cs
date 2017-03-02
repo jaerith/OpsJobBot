dca243ef48c353ab9c22ae344a4e7ac127f81b2b
jaerith <Aaron.Kendall@gmail.com>
2017-03-02 18:18:44 -0500

    * Added Ops Job Repo.

---
+++ OpsJobRepository/OpsRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsJobRepository
+{
+    public class OpsRepository
+    {
+        private static object InstanceLock = new object();
+        private static OpsRepository Instance = null;
+
+        private OpsRepository()
+        { }
+
+        static public OpsRepository GetInstance()
+        {
+            lock (InstanceLock)
+            {
+                Instance = new OpsRepository();
+            }
+
+            return Instance;
+        }
+
+        public HashSet<OpsJob> GetJobsList()
+        {
+            HashSet<OpsJob> AllJobs = new HashSet<OpsJob>();
+
+            AllJobs.Add(new OpsJob() { JobName = "job123", Description = "something", ActiveCd = "Y", LastReturnCd = "0" });
+            AllJobs.Add(new OpsJob() { JobName = "job456", Description = "blah", ActiveCd = "Y", LastReturnCd = "0" });
+            AllJobs.Add(new OpsJob() { JobName = "job789", Description = "etc", ActiveCd = "Y", LastReturnCd = "0" });
+
+            AllJobs.Add(new OpsJob() { JobName = "copyJob.bat", Description = "Copies...Duh", ActiveCd = "Y", LastReturnCd = "0" });
+
+            return AllJobs;
+        }
+    }
+}
